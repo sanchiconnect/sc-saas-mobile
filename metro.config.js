d597f2fab11daece76685f16ba3ad3e334069db2
vishalikashyap <vis48171@gmail.com>
2026-06-19 10:50:05 +0530

    metor update

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,16 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    blockList: [
+      // Exclude CMake / NDK build artefacts inside node_modules.
+      // These directories are created and deleted during the Android Gradle
+      // build, causing Metro's FallbackWatcher to crash with ENOENT.
+      /node_modules\/.*\/android\/\.cxx\/.*/,
+      /node_modules\/.*\/android\/build\/.*/,
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
